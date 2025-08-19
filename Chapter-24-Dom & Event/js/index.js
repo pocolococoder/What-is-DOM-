18ebd4a9a38d6e05af3fee13b4182681bd7f1b93
Rezwan Mahmud <rezwan.mahmud.007@gmail.com>
2025-08-19 19:59:43 +0600

    Add files via upload

---
+++ Chapter-24-Dom & Event/js/index.js
@@ -0,0 +1,38 @@
+// const liCollection = document.getElementsByTagName('li');
+// for(const li of liCollection){
+//     console.log(li.innerText);
+// }
+
+
+// const title = document.getElementById('fruits-title');
+// console.log(title.innerText);
+// title.innerText = 'Fruits I Love Most!'
+// console.log(title.innerText);
+
+// const title1 = document.getElementsByClassName('important-place');
+// for(const text of title1){
+//     console.log(text.innerText);
+// }
+
+// const fruitItems = document.querySelectorAll(".fruits-container li") ;
+// fruitItems.forEach(li=>console.log(li.innerText));
+
+// const fruitsTitle = document.querySelector('#fruits-title');
+// console.log(fruitsTitle.innerText);
+// fruitsTitle.innerText = 'Fruits I Hate Most!'
+// console.log(fruitsTitle.innerText);
+
+// const placesContainer = document.getElementById('vegetables-container');
+// console.log(placesContainer);
+// //all child nodes
+// console.log(placesContainer.childNodes);
+// //first child Node
+// console.log(placesContainer.firstChild);
+
+// const liT = document.createElement('li');
+// liT.innerText = 'Brand New Place To Go';
+// const placeUL = document.querySelector('#place-container ul');
+// placeUL.appendChild(liT);
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
